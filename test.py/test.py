76cee39e8b05e2d3af55c0852c0f9c85a812ff77
Gary Comins <gary.comins@hotmail.com>
2021-01-31 20:25:52 -0500

    Another test

--- test.py/test.py
+++ test.py/test.py
@@ -1,2 +1,34 @@
-print("Hello Git")
-print("Another change")
+import os
+import sys
+
+
+def getMenuItem():
+    switcher={
+            "0":'Exit',
+            "1":'Something'
+            }
+    
+    #Display the menu
+    os.system('cls')
+    print("PyOlympus v0.0")
+    print("______________________________________")
+    print("Menu Item - Description               ")
+    print("---------   --------------------------")
+    print(" 0          Exit Oympus               ")
+    print("______________________________________")
+    menu_selection = input("Select a menu option: ")
+
+    menu_selection = switcher.get(menu_selection,"Invalid Menu Selection")
+
+    return menu_selection
+
+
+#main menu
+
+#
+my_item = getMenuItem()
+if (my_item) == "Exit":
+    print("Closing application ... ")
+    sys.exit()
+else:
+    print(f"Implementing command: {my_item}")
